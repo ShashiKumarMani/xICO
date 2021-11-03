dfd5b5ba76c59b8cfbf28545218590a6f246edb6
ShashiKumarMani <shashikumarm052@gmail.com>
2021-11-03 21:05:04 +0530

    Add deploy scripts

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -3,7 +3,9 @@
 //
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
+const { ethers } = require("hardhat");
 const hre = require("hardhat");
+const { experimentalAddHardhatNetworkMessageTraceHook } = require("hardhat/config");
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
@@ -13,13 +15,21 @@ async function main() {
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
-  // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
 
-  await greeter.deployed();
+  const XToken = await hre.ethers.getContractFactory("XToken");
+  const xToken = await XToken.deploy("XToken", "EX", 50_000_000_000);
+  await xToken.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log(xToken.address);
+  const walletAddress = "";
+  const date = new Date();
+  const openingTime = Math.floor(date.getTime() / 1000) + 60;
+  const closingTime = openingTime + (30*24*60*60);
+  const priceFeed = "";
+
+  const XTokenSale = await hre.ethers.getContractFactory("XTokenSale");
+  const xTokenSale = await XTokenSale.deploy(1000, walletAddress, xToken.address, 12_500_000_000, openingTime, closingTime, priceFeed);
+  xTokenSale.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
