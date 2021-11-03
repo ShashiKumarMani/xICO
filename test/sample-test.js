ede47a48a0093ba60a5b253c5fed28d9253e2357
ShashiKumarMani <shashikumarm052@gmail.com>
2021-11-03 16:29:02 +0530

    Add tests for XTokenSale contract

--- test/sample-test.js
+++ test/sample-test.js
@@ -47,6 +47,8 @@ describe("xToken", function () {
 
     it("Mint Initial Tokens", async() => {
 
+      const [ , , account2, account3, account4, account5] = await ethers.getSigners();
+
       const RESERVE_WALLET = 15_000_000_000;
       const INTEREST_PAYOUT_WALLET = 10_000_000_000;
       const TEAM_MEMBERS_WALLET = 5_000_000_000;
@@ -54,11 +56,11 @@ describe("xToken", function () {
       const BOUNTIES_AIRDROP_WALLET = 1_000_000_000;
       const TOKENSALE_WALLET = 12_500_000_000;
 
-      await xToken.mint(owner.address, RESERVE_WALLET);
-      await xToken.mint(owner.address, INTEREST_PAYOUT_WALLET);
-      await xToken.mint(owner.address, TEAM_MEMBERS_WALLET);
-      await xToken.mint(owner.address, GENERAL_FUND_WALLET);
-      await xToken.mint(owner.address, BOUNTIES_AIRDROP_WALLET);
+      await xToken.mint(account1.address, RESERVE_WALLET);
+      await xToken.mint(account2.address, INTEREST_PAYOUT_WALLET);
+      await xToken.mint(account3.address, TEAM_MEMBERS_WALLET);
+      await xToken.mint(account4.address, GENERAL_FUND_WALLET);
+      await xToken.mint(account5.address, BOUNTIES_AIRDROP_WALLET);
       await xToken.mint(owner.address, TOKENSALE_WALLET);
 
       console.log("Total Supply",BigInt(await xToken.totalSupply()));
@@ -80,3 +82,43 @@ describe("xToken", function () {
     });
   });
 });
+
+
+describe("XTokenSale", function() {
+  let xToken;
+  let xTokenSale;
+  let owner;
+  let account1;
+
+  beforeEach(async () => {
+    const XToken = await ethers.getContractFactory("XToken");
+    xToken = await XToken.deploy("xToken", "EX", 50_000_000_000);
+    let res = await xToken.deployed();
+    [owner, account1] = await ethers.getSigners();
+    const date = new Date();
+
+    // One hour after deployment incl mining
+    let open = Math.floor(date.getTime() / 1000) + (60 * 60);
+    let close = open + (30*24*60*60);
+
+    const XTokenSale = await ethers.getContractFactory("XTokenSale");
+    // Aggregator not used
+    xTokenSale = await XTokenSale.deploy(1000, account1.address, xToken.address, 12_500_000_000, open, close, xToken.address);
+    await xTokenSale.deployed();
+  });
+
+  describe("Constructor", async() => {
+
+    it("Cap", async () => {
+      expect(await xTokenSale.cap()).to.be.equal(12_500_000_000);
+    });
+  });
+
+  describe("Open", () => {
+
+    // opens after 60*60 seconds
+    it("open false", async () => {
+        expect(await xTokenSale.isOpen()).to.be.equal(false);
+    });
+  })
+}); 
